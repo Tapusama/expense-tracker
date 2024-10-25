9be1c7a8e4783c7745ae3b7ccc216cc2ebf0780f
ORSAC <prasnajit.pradhan@orsac.team>
2024-10-25 19:57:03 +0530

    fixes

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,8 +1,11 @@
 "use client"; // This file is a client component
-import { useState } from 'react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'; // Icons for menu and close
+import { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"; // Icons for menu and close
+import { FaWallet } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import { IoMdCash } from "react-icons/io";
 
-const Sidebar = ({children}) => {
+const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -12,30 +15,142 @@ const Sidebar = ({children}) => {
   return (
     <div className="flex">
       {/* Sidebar */}
-      <div className={`fixed inset-y-0 left-0 z-50 w-64 bg-gray-800 text-white transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out md:relative md:translate-x-0`}>
+      <div
+        // className={`fixed inset-y-0 left-0 z-50 w-64 bg-gray-800 text-white transform ${
+        //   isOpen ? "translate-x-0" : "-translate-x-full"
+        // } transition-transform duration-300 ease-in-out md:relative md:translate-x-0`}
+        className="fixed inset-y-0 left-0 z-50 w-64 bg-gray-800 text-white"
+      >
         <div className="p-4">
-          <h2 className="text-2xl font-bold">Sidebar</h2>
+          <h2 className="text-2xl font-bold">
+            <button
+              onClick={toggleSidebar}
+              className="text-gray-500 focus:outline-none m-2 flex items-center p-2"
+            >
+              <AiOutlineMenu size={24} />
+              {isOpen ? <span className="ms-3">Menu</span> : <></>}
+            </button>
+          </h2>
+
           <ul className="mt-4 space-y-2">
-            <li className="hover:bg-gray-700 p-2 rounded"><a href="/Dashboard">Dashboard</a></li>
-            <li className="hover:bg-gray-700 p-2 rounded"><a href="/Wallet">Wallet</a></li>
-            <li className="hover:bg-gray-700 p-2 rounded"><a href="/Budget">Budget</a></li>
-            <li className="hover:bg-gray-700 p-2 rounded"><a href="/Analytics">Analytics</a></li>
-            <li className="hover:bg-gray-700 p-2 rounded"><a href="/Expenses">Expenses</a></li>
-            <li className="hover:bg-gray-700 p-2 rounded"><a href="/Settings">Settings</a></li>
+            <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Dashboard" className="flex items-center p-2">
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Dashboard</span>
+              </a>
+            </li>
+
+            <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Wallet" className="flex items-center p-2">
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Wallet</span>
+              </a>
+            </li>
+
+            <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Budget" className="flex items-center p-2">
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Budget</span>
+              </a>
+            </li>
+
+            <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Analytics" className="flex items-center p-2">
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Analytics</span>
+              </a>
+            </li>
+
+            <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Expenses" className="flex items-center p-2">
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Expenses</span>
+              </a>
+            </li>
+
+            <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Settings" className="flex items-center p-2">
+                <svg
+                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Settings</span>
+              </a>
+            </li>
+            {/* <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Wallet">Wallet</a>
+            </li> */}
+            {/* <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Budget">Budget</a>
+            </li> */}
+            {/* <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Analytics">Analytics</a>
+            </li> */}
+            {/* <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Expenses">Expenses</a>
+            </li> */}
+            {/* <li className="hover:bg-gray-700 p-2 rounded">
+              <a href="/Settings">Settings</a>
+            </li> */}
           </ul>
         </div>
       </div>
 
       {/* Content and Sidebar toggle for mobile */}
       <div className="flex-1 p-4 md:ml-64">
-        <div className="md:hidden">
-          <button onClick={toggleSidebar} className="text-gray-500 focus:outline-none">
-            {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
-          </button>
-        </div>
-
         {/* Main content */}
-        <div className="mt-4">
+        <div className="mt-4" style={{ height: "800px" }}>
           {children}
         </div>
       </div>
