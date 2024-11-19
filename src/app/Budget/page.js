a22e1cffc2a0a030d3dff14eda020d2a24ec4106
ORSAC <prasnajit.pradhan@orsac.team>
2024-11-19 17:48:15 +0530

    fixes

--- src/app/Budget/page.js
+++ src/app/Budget/page.js
@@ -1,9 +1,144 @@
+"use client";
 import React, { memo } from "react";
+import "./budget.css";
+import CustomCards from "@/components/Customcards/CustomCards";
+import Title from "@/components/Title/Title";
+import HighChartLineGraph from "@/components/Highcharts/HighChartLine";
+import { CiSquarePlus } from "react-icons/ci";
 
 const Budget = memo((props) => {
+  let BalanceTrendssData = [
+    { name: "JAN", y: 3000 },
+    { name: "FEB", y: 5474 },
+    { name: "MAR", y: 4555 },
+    { name: "APR", y: 6034 },
+    { name: "MAY", y: 2103 },
+    { name: "JUN", y: 3023 },
+    { name: "JUL", y: 1107 },
+    { name: "AUG", y: 1506 },
+    { name: "SEP", y: 2505 },
+    { name: "OCT", y: 4504 },
+    { name: "NOV", y: 3504 },
+    { name: "DEC", y: 5504 },
+  ];
+
+  const propsForLinechart = {
+    title: "Budget Period",
+    series: [
+      {
+        name: "Target (Rs.)",
+        data: BalanceTrendssData,
+        color: "#7271e9", // Line color
+        fillColor: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [
+            [0, "rgb(239,239,253,0.5)"], // Gradient start (Area color)
+            [1, "rgb(239,239,253, 0.5)"], // Gradient end (transparent)
+          ],
+        },
+      },
+    ],
+    titleY: " ",
+    drillDownDataSeries: [],
+    type: "area",
+    // onClickviewHandler,
+    // inverted,
+    // height: "300px",
+    // Options,
+    // styles: { fontFamily: "sans-serif", color: "#000", fontWeight: "bold",background:"red" },
+    header: "Month",
+    unit: "Rs.",
+  };
+
+  const propsForTitle = {
+    title: "Grocery",
+  };
+  const propsAddMore = {
+    title: "Add new wallet",
+    titleTextClass: "text-sm text-[#1f2c73]",
+    moreLink: <CiSquarePlus color="#1f2c73" size={20} />
+  };
+  const propsForPersonalFunds = {
+    className:
+      "border border-gray-100 shadow-md rounded-md bg-white flex-col w-full p-6",
+    hr: true,
+    title: "Total Balance",
+    moreLink: "VISA",
+    subFooterLeft: "Personal Funds",
+    subFooterRight: "$32,500.28",
+    subTitle: "$221,478",
+    titleClass: "text-sm text-[#7184ad]",
+    subTitleClass: "text-3xl text-[#1f2c73]",
+    subfooterClassLeft: "text-sm text-[#7184ad]",
+    subfooterClassRight: "font-semibold text-md text-[#1f2c73]",
+    footerClassLeft: "text-sm text-[#7184ad]",
+    footerClassRight: "font-semibold text-md text-[#1f2c73]",
+    moreLink: "",
+    subFooter: "",
+    cardBody: "",
+    footerLeft: "Credit Limits",
+    footerRight: "$2500.00",
+  };
+
+  const propsForTotalBalance = {
+    className:
+      "border border-gray-100 shadow-md rounded-md bg-white flex-col w-full p-6",
+    title: "Total Balance",
+    amount: 245860,
+    lastMonthAnt: 24478,
+    type: "increase",
+    percentage: "2.47%",
+  };
+
+  const propsForDebitCard = {
+    className: "debitCard",
+    hr: false,
+    title: "Debit Card",
+    moreLink: "VISA",
+    footerLeft: "User Name",
+    footerRight: "EXP:12/29",
+    subTitle: "",
+    titleClass: "font-semibold text-md text-[#7184ad]",
+    subTitleClass: "",
+    subfooterClass: "",
+    footerClass: "",
+    moreLink: "",
+    subFooter: "",
+    cardBody: "1234 5678 8906 7777",
+    footerLeft: "User name",
+    footerRight: "Exp:mm/yy",
+  };
+
+  const propsForMonthlyExpenses = {
+    className:
+      "border border-gray-100 shadow-md rounded-md bg-white flex-col w-full p-6",
+    title: "Monthly Expenses",
+    amount: 245860,
+    lastMonthAnt: 24478,
+    type: "increase",
+    percentage: "2.47%",
+  };
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Budget!</h1>
+    <div className="bg-rgba(215, 226, 247, 0.48) flex flex-col container mx-auto py-10 gap-6">
+      <div>
+        <h3 className="text-xl font-semibold text-[#1e1b4b]">Wallets</h3>
+        <p className="text-sm py-2 text-[#8c8baf]">
+          You can manage your Budget and Estimates here.
+        </p>
+      </div>
+      <div className="flex flex-row container h-full gap-6">
+        <div className="budgetLeftContainer">
+          <CustomCards />
+          <CustomCards />
+          <CustomCards />
+          <CustomCards />
+          <Title {...propsAddMore} />
+        </div>
+        <div className="budgetRightContainer">
+          <Title {...propsForTitle} />
+          <HighChartLineGraph {...propsForLinechart} />
+        </div>
+      </div>
     </div>
   );
 });
